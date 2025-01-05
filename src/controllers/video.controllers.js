b95dad1fc07eafd5eb7809f191dd1129d60513ff
Deepu <deepuemio@gmail.com>
2025-01-05 11:54:02 +0530

    add get all controller

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -65,8 +65,8 @@ const getVideo = asyncHandler(async(req,res)=>{
     )
 })
 
-const getAllVideos = asyncHandler(async(req,res)=>{
-    
-})
+const getAllVideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+}
 
 export { publishVideo, getVideo }
