4b9667550545cbd1351e63227911434aee92004a
Deepu <deepuemio@gmail.com>
2025-01-08 19:30:19 +0530

    add routes for tweet controllers

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,1 +1,18 @@
 import { Router } from "express";
+import {
+    addTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+} from "../controllers/tweet.controllers.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router()
+
+router.route("/")
+    .post(verifyJWT,addTweet)
+    .get(verifyJWT,getUserTweets)
+    .patch(verifyJWT,updateTweet)
+    .delete(verifyJWT,deleteTweet)
+
+export default router
