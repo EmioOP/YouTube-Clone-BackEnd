a34fdfe889b8d07d759bbfb30da3b7b7c9af9620
Deepu <deepuemio@gmail.com>
2025-01-08 18:41:10 +0530

    complete video routes and controllers

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,21 +1,32 @@
 import { Router } from "express";
-import { publishVideo } from "../controllers/video.controllers,js";
+import {
+    publishVideo,
+    getVideo,
+    getAllVideos,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus
+} from "../controllers/video.controllers,js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { getVideo } from "../controllers/video.controllers.js";
 
 
-const router =  Router()
 
-router.route("/video").post(verifyJWT,upload.fields(
+const router = Router()
+
+router.route("/video").post(verifyJWT, upload.fields(
     [
-        {name:"videoFile",maxCount:1},
-        {name:"thumbnail",maxCount:1}
-    ]),publishVideo)
+        { name: "videoFile", maxCount: 1 },
+        { name: "thumbnail", maxCount: 1 }
+    ]), publishVideo)
 
-router.route("/:videoId").get(getVideo)
+router.route("/").get(getAllVideos) // use verifyJWT middleware for checking user is loged in or not
 
+router.route("/:videoId")
+    .get(getVideo)
+    .patch(verifyJWT, updateVideo)
+    .delete(verifyJWT, deleteVideo)
 
-
+router.route("/:videoId/toggle-publish").patch(verifyJWT, togglePublishStatus)
 
 
 export default router
